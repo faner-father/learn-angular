f7b2f0a12fd86a29bbc1368a05de7752844745c8
cloud <fcq@fortunes.com.cn>
2015-08-18 15:01:51 +0800

    add nested page

--- app/ng-ui-route/app.js
+++ app/ng-ui-route/app.js
@@ -19,6 +19,23 @@ routerApp.config(function($stateProvider, $urlRouterProvider) {
         // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
         .state('about', {
             // we'll get to this in a bit
-        });
+        })
+
+        // nested list with custom controller
+        .state('home.list', {
+            url: '/list',
+            templateUrl: 'partial-home-list.html',
+            controller: function($scope) {
+                $scope.dogs = ['Bernese', 'Husky', 'Goldendoodle'];
+            }
+        })
+
+        // nested list with just some random string data
+        .state('home.paragraph', {
+            url: '/paragraph',
+            template: 'I could sure use a drink right now.'
+        })
+    ;
+
 
 });
